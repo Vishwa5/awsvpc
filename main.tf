2b7a08e04fdeb6a0e577d431e26933b59d9bf513
Vishwa5 <49957473+Vishwa5@users.noreply.github.com>
2019-09-26 19:22:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,9 @@ locals {
     length(var.redshift_subnets),
   )
   nat_gateway_count = var.single_nat_gateway ? 1 : var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length
-
+  
   # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
-  vpc_id = element(
-    concat(
-      aws_vpc_ipv4_cidr_block_association.this.*.vpc_id,
-      aws_vpc.this.*.id,
-      [""],
-    ),
-    0,
-  )
-
-  vpce_tags = merge(
-    var.tags,
-    var.vpc_endpoint_tags,
-  )
+  vpc_id = {"var.vpc_id"}
 }
 
 ######
